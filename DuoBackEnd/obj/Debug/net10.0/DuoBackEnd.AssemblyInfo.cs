a74e57e4e0d8405a0a8607661356c89098b95157
Gabriel17Br <falljamm@gmail.com>
2026-06-18 07:40:25 -0300

    hjhj

--- DuoBackEnd/obj/Debug/net10.0/DuoBackEnd.AssemblyInfo.cs
+++ DuoBackEnd/obj/Debug/net10.0/DuoBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuoBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+363fb52dbc44eee3e2b40ed7ca91098e2f0d2bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acdc87ad6d16bb842533154f457e7b88f20ba9af")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuoBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuoBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
